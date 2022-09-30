1a2ebda2d4374e83160d40b866224b01324702ea
Aneesh Agarwal <911aneesh@gmail.com>
2022-09-30 16:21:08 -0700

    loop change

--- welcome/code/olleh.cpp
+++ welcome/code/olleh.cpp
@@ -6,7 +6,7 @@ using namespace std;
 string reverseWord(string word)
 {
     string rev = "";
-    for (int i = word.length(); i >= 0; i--)
+    for (int i = word.length() - 1; i >= 0; i--)
     {
         rev = rev + word[i];
     }
