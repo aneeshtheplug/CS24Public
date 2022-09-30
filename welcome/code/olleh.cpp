fb9691ec5c55e424caad21e8716f9dda1b861a68
Aneesh Agarwal <911aneesh@gmail.com>
2022-09-30 12:54:14 -0700

    New folder

---
+++ welcome/code/olleh.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string reverseWord(string word)
+{
+    string rev = "";
+    for (int i = word.length(); i >= 0; i--)
+    {
+        rev = rev + word[i];
+    }
+    return rev;
+}
+
+int main()
+{
+    string input;
+    string word = "";
+
+    getline(cin, input);
+
+    int length = input.length();
+    for (int i = 0; i < length; i++)
+    {
+        if (i == length - 1)
+        {
+            if (isspace(input[i]) || ispunct(input[i]))
+            {
+                cout << reverseWord(word) << input[i] << endl;
+                word = "";
+            }
+            else
+            {
+                word = word + input[i];
+                cout << reverseWord(word) << endl;
+            }
+        }
+        else if (isspace(input[i]) || ispunct(input[i]))
+        {
+            cout << reverseWord(word) << input[i];
+            word = "";
+        }
+        else
+        {
+            word = word + input[i];
+        }
+    }
+}
