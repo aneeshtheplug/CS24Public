c8e18ad93941f6678709f9fbc0c2df727a19f663
Aneesh Agarwal <911aneesh@gmail.com>
2022-10-04 10:52:58 -0700

    member functions

--- fibvec/FibVec.cpp
+++ fibvec/FibVec.cpp
@@ -1,2 +1,36 @@
 #include <iostream>
 #include "FibVec.h"
+#include <vector>
+using namespace std;
+
+FibVec::FibVec()
+{
+    vector<int> fib(1);
+}
+
+int FibVec::count() const
+{
+}
+
+void FibVec::insert(int value, int index)
+{
+}
+
+int FibVec::lookup(int index) const
+{
+}
+
+void FibVec::pop()
+{
+}
+
+void FibVec::push(int value)
+{
+}
+int FibVec::remove(int index)
+{
+}
+
+FibVec FibVec::slice(int index, int count)
+{
+}
