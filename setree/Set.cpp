8929d2ec809542cc9ab330a66086be243b03bc9d
Aneesh Agarwal <911aneesh@gmail.com>
2022-10-20 11:18:08 -0700

    remove contains

--- setree/Set.cpp
+++ setree/Set.cpp
@@ -241,10 +241,10 @@ Node *removeHelp(Node *current, const string &value)
 
 size_t Set::remove(const string &value)
 {
-    if (contains(value) == false)
-    {
-        return 0;
-    }
+    // if (contains(value) == false)
+    // {
+    //     return 0;
+    // }
     size_t prevCount = count();
     removeHelp(mRoot, value);
     size_t afterCount = count();
