bd0ec7f42132bd47f0111e7e36c0ee067a83f9ce
Aneesh Agarwal <911aneesh@gmail.com>
2022-11-09 00:38:19 -0800

    last

--- typo/Dictionary.cpp
+++ typo/Dictionary.cpp
@@ -1,27 +1,72 @@
 #include "Dictionary.h"
+#include <cmath>
 using namespace std;
+
+bool isLower(string word)
+{
+    for (size_t i = 0; i < word.length(); i++)
+    {
+        if (islower(word[i]) == true)
+        {
+            continue;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    return true;
+}
 
 Dictionary::Dictionary(std::istream &stream)
 {
     std::string line;
     while (std::getline(stream, line))
     {
-        if (line[0] == '#' || line.empty())
-        {
-            continue;
-        }
-        else
+        if (isLower(line))
         {
             mWords.push_back(line);
         }
     }
 }
 
+float distanceCalc(string str, const std::vector<Point> &ptr)
+{
+    float match = 0;
+    size_t i = 0;
+    for (auto itr = ptr.begin(); itr != ptr.end(); itr++)
+    {
+        int index = (str[i] - 97);
+        float distance = sqrt(pow((QWERTY[index].x - itr->x), 2) + pow((QWERTY[index].y - itr->y), 2));
+        float score = 1 / (10 * pow(distance, 2) + 1);
+        match = match + score;
+        i++;
+    }
+    float avg = match / str.length();
+    return avg;
+}
+
 Heap Dictionary::correct(const std::vector<Point> &points, size_t maxcount, float cutoff) const
 {
-    cutoff++;
-    maxcount++;
-    Heap deez(7);
-    return deez;
-    points.capacity();
+    Heap heap(maxcount);
+    size_t len = points.size();
+    std::vector<std::string> compare;
+    auto itr = mWords.begin();
+    while (itr != mWords.end())
+    {
+        if ((*itr).length() == len)
+        {
+            compare.push_back(*itr);
+        }
+        ++itr;
+    }
+    for (auto it = compare.begin(); it != compare.end(); it++)
+    {
+        float score = distanceCalc(*it, points);
+        if (score >= cutoff)
+        {
+            heap.push(*it, score);
+        }
+    }
+    return heap;
 }
